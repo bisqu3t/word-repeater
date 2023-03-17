375787b1f59887b9eb0eb9d14025f2563c9b3376
cameronmarshall <cmarshall921@icloud.com>
2023-03-17 13:27:42 -0600

    #fixed n-1 issue

--- word-repeater.py
+++ word-repeater.py
@@ -3,9 +3,9 @@ def main():
     Take in a word and an integer and repeat the word n times
     """
     word = input("Enter word to repeat: ")
-    n = int(input("Enter number of times to repeat: "))
-
-    for i in range(1, n):
+    n = int(input("Enter number of times to repeat:  "))
+#it went from 1 to 0
+    for i in range(0, n):
         print(word)
 
 
